7be054a3ae04b5a3840bceb729df5110dea29322
onni <holttaonni@gmail.com>
2026-03-17 08:50:06 +0200

    Update index.js

--- 4.2 tehtävä/blogi/index.js
+++ 4.2 tehtävä/blogi/index.js
@@ -1,7 +1,5 @@
-import express from 'express'
 import dotenv from 'dotenv'
 import mongoose from 'mongoose'
-import Blog from './src/blogstuff/blog.js'
 import app from './src/app.js'
 
 dotenv.config()
