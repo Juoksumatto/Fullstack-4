28ee3d7db84d22107aeaf2545d330150ecad0101
onni <holttaonni@gmail.com>
2026-03-24 13:51:31 +0200

    Add files via upload

---
+++ 4.12 tehtävä/blogi/src/app.js
@@ -0,0 +1,23 @@
+import express from 'express'
+import Blog from './blogstuff/blog.js'
+
+const app = express()
+
+app.use(express.json())
+
+app.get('/api/blogs', async (req, res) => {
+  const blogs = await Blog.find({})
+  res.json(blogs)
+})
+
+app.post('/api/blogs', async (req, res) => {
+  const { title, url } = req.body
+  if (!title || !url) {
+    return res.status(400).json({ error: 'title or url missing' })
+  }
+  const blog = new Blog(req.body)
+  const savedBlog = await blog.save()
+  res.status(201).json(savedBlog)
+})
+
+export default app
