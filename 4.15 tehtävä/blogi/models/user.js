6cd35f6eddaa7c65977b8ccd2f004a16a7b2d3c1
onni <holttaonni@gmail.com>
2026-03-30 13:07:17 +0300

    Update user.js

--- 4.15 tehtävä/blogi/models/user.js
+++ 4.15 tehtävä/blogi/models/user.js
@@ -3,9 +3,7 @@ import mongoose from 'mongoose'
 const userSchema = new mongoose.Schema({
   username: {
     type: String,
-    required: true,
-    unique: true,
-    minlength: 3
+    required: true
   },
   name: {
     type: String,
