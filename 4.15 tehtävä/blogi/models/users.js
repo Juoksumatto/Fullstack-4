5e14f22f4c4e7a1165c049fd68ba650f0a74925a
onni <holttaonni@gmail.com>
2026-03-30 11:25:39 +0300

    Update users.js

--- 4.15 tehtävä/blogi/models/users.js
+++ 4.15 tehtävä/blogi/models/users.js
@@ -15,10 +15,6 @@ usersRouter.post('/', async (req, res) => {
     return res.status(400).json({ error: 'username and password are required' })
   }
 
-  if (username.length < 3 || password.length < 3) {
-    return res.status(400).json({ error: 'username and password must be at least 3 characters long' })
-  }
-
   const saltRounds = 10
   const passwordHash = await bcrypt.hash(password, saltRounds)
 
